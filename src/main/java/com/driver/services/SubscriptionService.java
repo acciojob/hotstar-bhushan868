82945aab6193a8fbf0b70ed22d056305cd0331f4
bhushan868 <bhushukarale643@gmail.com>
2023-07-04 22:24:01 +0530

    commited

--- src/main/java/com/driver/services/SubscriptionService.java
+++ src/main/java/com/driver/services/SubscriptionService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SubscriptionService {
@@ -26,7 +27,48 @@ public class SubscriptionService {
 
         //Save The subscription Object into the Db and return the total Amount that user has to pay
 
-        return null;
+        //code
+        //getting user by id
+        User user = userRepository.findById(subscriptionEntryDto.getUserId()).get();
+        if (user==null)return 0;
+
+        Subscription userSubscription = new Subscription();
+        userSubscription.setSubscriptionType(subscriptionEntryDto.getSubscriptionType());
+//        userSubscription.setStartSubscriptionDate(new Date());
+        userSubscription.setNoOfScreensSubscribed(subscriptionEntryDto.getNoOfScreensRequired());
+
+        int amountPaid = 0;
+
+        int screenSubscribed = subscriptionEntryDto.getNoOfScreensRequired();
+
+        //Basic : 500 + 200 * noOfScreensSubscribed
+        if(subscriptionEntryDto.getSubscriptionType().equals(SubscriptionType.BASIC))
+        {
+            amountPaid = 500 + (200 * screenSubscribed);
+        }
+        //Pro : 800 + 250*noOfScreensSubscribed
+        else if(subscriptionEntryDto.getSubscriptionType().equals(SubscriptionType.PRO))
+        {
+            amountPaid = 800 + (250 * screenSubscribed);
+        }
+        //ELITE Plan : 1000 + 350*noOfScreensSubscribed
+        else if(subscriptionEntryDto.getSubscriptionType().equals(SubscriptionType.ELITE))
+        {
+            amountPaid = 1000 + (350 * screenSubscribed);
+        }
+        else
+            return 0;
+
+        userSubscription.setTotalAmountPaid(amountPaid);
+
+        //mapping user->subscription
+        user.setSubscription(userSubscription);
+
+        subscriptionRepository.save(userSubscription);
+
+        userRepository.save(user);
+
+        return amountPaid;
     }
 
     public Integer upgradeSubscription(Integer userId)throws Exception{
@@ -34,8 +76,45 @@ public class SubscriptionService {
         //If you are already at an ElITE subscription : then throw Exception ("Already the best Subscription")
         //In all other cases just try to upgrade the subscription and tell the difference of price that user has to pay
         //update the subscription in the repository
+        Optional<User> userOptional = userRepository.findById(userId);
+        if(!userOptional.isPresent())
+            return 0;
 
-        return null;
+        User user = userOptional.get();
+
+        if(user.getSubscription().getSubscriptionType().equals(SubscriptionType.ELITE))
+        {
+            throw new Exception("Already the best Subscription");
+        }
+
+        Subscription subscription = user.getSubscription();
+
+        int screenSubscribed = subscription.getNoOfScreensSubscribed();
+
+        int totalAmountBefore = subscription.getTotalAmountPaid();
+
+        int amountDiff=0;
+        int updatedAmount=0;
+
+        if(subscription.getSubscriptionType().equals(SubscriptionType.PRO))
+        {
+            updatedAmount = 1000 + (350 * screenSubscribed);
+            subscription.setSubscriptionType(SubscriptionType.ELITE);
+            subscription.setTotalAmountPaid(updatedAmount);
+        }
+        else if(subscription.getSubscriptionType().equals(SubscriptionType.BASIC))
+        {
+            updatedAmount = 800 + (250 * screenSubscribed);
+            subscription.setSubscriptionType(SubscriptionType.PRO);
+            subscription.setTotalAmountPaid(updatedAmount);
+        }
+
+        user.setSubscription(subscription);
+        userRepository.save(user);
+        subscriptionRepository.save(subscription);
+
+        amountDiff = updatedAmount-totalAmountBefore;
+        return amountDiff;
     }
 
     public Integer calculateTotalRevenueOfHotstar(){
@@ -43,7 +122,16 @@ public class SubscriptionService {
         //We need to find out total Revenue of hotstar : from all the subscriptions combined
         //Hint is to use findAll function from the SubscriptionDb
 
-        return null;
+        List<Subscription> subscriptionList = subscriptionRepository.findAll();
+        if(subscriptionList.isEmpty())
+            return 0;
+        int totalRevenue=0;
+        for(Subscription subscription : subscriptionList)
+        {
+            totalRevenue = totalRevenue + subscription.getTotalAmountPaid();
+        }
+
+        return totalRevenue;
     }
 
 }
--- src/main/java/com/driver/services/UserService.java
+++ src/main/java/com/driver/services/UserService.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -24,8 +25,14 @@ public class UserService {
 
     public Integer addUser(User user){
 
-        //Jut simply add the user to the Db and return the userId returned by the repository
-        return null;
+        User newUser = new User();
+        newUser.setName(user.getName());
+        newUser.setAge(user.getAge());
+        newUser.setMobNo(user.getMobNo());
+        newUser = userRepository.save(newUser);
+        return newUser.getId();
+
+        //Just simply add the user to the Db and return the userId returned by the repository
     }
 
     public Integer getAvailableCountOfWebSeriesViewable(Integer userId){
@@ -33,8 +40,51 @@ public class UserService {
         //Return the count of all webSeries that a user can watch based on his ageLimit and subscriptionType
         //Hint: Take out all the Webseries from the WebRepository
 
+        List<WebSeries> webSeriesList = webSeriesRepository.findAll();
 
-        return null;
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (!userOptional.isPresent())
+            return 0;
+
+        User user = userOptional.get();
+
+        int countSeries = 0;
+
+        for(WebSeries webSeries : webSeriesList)
+        {
+//            if (webSeries.getSubscriptionType().equals(user.getSubscription().getSubscriptionType()) && webSeries.getAgeLimit() <= user.getAge())
+//            {
+//                countSeries+=1;
+//            }
+            if(user.getSubscription().getSubscriptionType().equals(SubscriptionType.ELITE))
+            {
+                if(user.getAge()>=webSeries.getAgeLimit())
+                {
+                    countSeries++;
+                }
+            }
+            else if(user.getSubscription().getSubscriptionType().equals(SubscriptionType.PRO))
+            {
+                if(webSeries.getSubscriptionType().equals(SubscriptionType.BASIC) || webSeries.getSubscriptionType().equals(SubscriptionType.PRO))
+                {
+                    if(user.getAge()>=webSeries.getAgeLimit())
+                    {
+                        countSeries++;
+                    }
+                }
+            }
+            else {
+                if(user.getSubscription().getSubscriptionType().equals(SubscriptionType.BASIC) && webSeries.getSubscriptionType().equals(SubscriptionType.BASIC))
+                {
+                    if(user.getAge()>=webSeries.getAgeLimit())
+                    {
+                        countSeries++;
+                    }
+                }
+            }
+        }
+
+        return countSeries;
     }
 
 
